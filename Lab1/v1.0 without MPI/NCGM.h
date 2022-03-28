45c876f6464213db07d7f806eef0511a1d516cd4
Chaos Sukhinsky <kos368437@yandex.ru>
2022-03-28 17:14:28 +0700

    Delete obsolette files

--- Lab1/v1.0 without MPI/NCGM.h
+++
@@ -1,7 +0,0 @@
-#ifndef LAB1_NCGM_H
-#define LAB1_NCGM_H
-#include "../Matrix.h"
-
-void solveSystemUsingNCGM(Matrix A, Matrix x, Matrix b, double eps, int * counter);
-
-#endif //LAB1_NCGM_H
--- Lab1/v1.1 with MPI, duplicated x and b/NCGMP.h
+++
@@ -1,9 +0,0 @@
-#ifndef LAB1_NCGM_H
-#define LAB1_NCGM_H
-#include "../Matrix.h"
-//#include <mpi.h>
-
-void solveSystemUsingNCGM(Matrix A, Matrix x, Matrix b, double eps, int * counter);
-void parallelMultiplyMatrix(Matrix dest, Matrix first, Matrix second, Matrix tempResult);
-
-#endif //LAB1_NCGM_H
