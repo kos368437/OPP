6ac7b5f600a828e886fc9171da80bfefb2028c62
Chaos Sukhinsky <kos368437@yandex.ru>
2022-07-06 20:53:45 +0700

    Added multythreading task delivery

--- Lab5/main.c
+++ Lab5/main.c
@@ -12,12 +12,18 @@ typedef struct s_TaskList {
     pthread_mutex_t mutex_taskList;
 }TaskList;
 
+#define TAG_REQUEST 42
+#define TAG_ANSWER 420
+#define QUIT_THREAD (-1)
+#define MIN_REMAINED_TASKS_TO_START_SHARING 5
+
 void createTaskList(TaskList * taskList, unsigned int newListLength);
 void initTaskList(TaskList * taskList, int commRank, int commSize, unsigned int iterNum);
 void createAndStartTaskPullThread(pthread_t *thread_taskPull, TaskList * taskList);
-double doTasks(TaskList *taskList, double *workTime_return);
-void * pullTasks(void * taskListPointer);
+double doTasks(TaskList *taskList, double *workTime_return, int commRank, int commSize);
+void * sendTasks(void * taskListPointer);
 void deleteTaskList(TaskList * taskList);
+int askForExtraTasks(TaskList *taskList, int commRank, int commSize);
 
 int main(int argc, char * argv[]) {
     int required = MPI_THREAD_MULTIPLE;
@@ -25,10 +31,13 @@ int main(int argc, char * argv[]) {
     double result;
 
     struct timespec tm_start, tm_finish;
-    double best_time = 0;
     double totalTime;
     double jobTime = 0;
+    double minJobTime = 0;
+    double maxJobTime = 0;
+
     int commRank, commSize;
+    int quitMessage = QUIT_THREAD;
 
     TaskList taskList;
 
@@ -37,7 +46,7 @@ int main(int argc, char * argv[]) {
 
     MPI_Init_thread(&argc, &argv, required, &provided);
 
-    pthread_t thread_taskPull;
+    pthread_t thread_taskSend;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &commRank);
     MPI_Comm_size(MPI_COMM_WORLD, &commSize);
@@ -50,21 +59,32 @@ int main(int argc, char * argv[]) {
 
     createTaskList(&taskList, taskListLengthPerProcess);
 
-    createAndStartTaskPullThread(&thread_taskPull, &taskList);
+    createAndStartTaskPullThread(&thread_taskSend, &taskList);
 
     clock_gettime(CLOCK_REALTIME, &tm_start);
 
     for (int i = 0; i < numOfWaves; i++) {
+        if (commRank == 0) printf("\nWave %d\n", i);
+
         initTaskList(&taskList, commRank, commSize, i);
-        result = doTasks(&taskList, &jobTime);
+        MPI_Barrier(MPI_COMM_WORLD);
+        result = doTasks(&taskList, &jobTime, commRank, commSize);
+
+        MPI_Allreduce(&jobTime,&minJobTime,1,MPI_DOUBLE,MPI_MIN,MPI_COMM_WORLD);
+        MPI_Allreduce(&jobTime,&maxJobTime,1,MPI_DOUBLE,MPI_MAX,MPI_COMM_WORLD);
+
         if (commRank == 0) {
-            printf("Wave %d\n %lf\n", i, jobTime);
+            printf("MIN Time\n%lf\n", minJobTime);
+            printf("MAX Time\n%lf\n", maxJobTime);
+            printf("Imbalance\n%lf\n", maxJobTime - minJobTime);
+            printf("Imbalance %%\n%lf\n", ((maxJobTime - minJobTime) / maxJobTime) * 100);
         }
     }
 
     clock_gettime(CLOCK_REALTIME, &tm_finish);
 
-    if (0 != pthread_join(thread_taskPull, NULL)) {
+    MPI_Send(&quitMessage, 1, MPI_INT, commRank, TAG_REQUEST, MPI_COMM_WORLD);
+    if (0 != pthread_join(thread_taskSend, NULL)) {
         perror("Cannot join a thread");
         MPI_Finalize();
         abort();
@@ -73,7 +93,7 @@ int main(int argc, char * argv[]) {
     totalTime = tm_finish.tv_sec - tm_start.tv_sec + 1e-9 * (tm_finish.tv_nsec - tm_start.tv_nsec);
 
     if (commRank == 0) {
-        printf("Total time:\n%lf\n", totalTime);
+        printf("\nTOTAL TIME:\n%lf\n", totalTime);
     }
     deleteTaskList(&taskList);
 
@@ -82,27 +102,61 @@ int main(int argc, char * argv[]) {
     return 0;
 }
 
-double doTasks(TaskList *taskList, double *workTime_return) {
+double doTasks(TaskList *taskList, double *workTime_return, int commRank, int commSize) {
     double time;
     struct timespec tm_start, tm_finish;
     int i = 0;
     double result = 0;
+    int remainedTasksCount;
+
+    pthread_mutex_lock(&(taskList->mutex_taskList));
+    remainedTasksCount = taskList->remainedTasksCount;
+    pthread_mutex_unlock(&(taskList->mutex_taskList));
 
     clock_gettime(CLOCK_REALTIME, &tm_start);
 
-    while (taskList->remainedTasksCount > 0) {
-        for (int j = 0; j < taskList->taskList[i]; j++) {
-            result += sin(j);
+    while (1) {
+        i = 0;
+        while (remainedTasksCount > 0) {
+            for (int j = 0; j < taskList->taskList[i]; j++) {
+                result += sin(j);
+            }
+            i++;
+            remainedTasksCount -= 1;
         }
-        i++;
         pthread_mutex_lock(&(taskList->mutex_taskList));
-        taskList->remainedTasksCount -= 1;
+        taskList->remainedTasksCount = remainedTasksCount;
         pthread_mutex_unlock(&(taskList->mutex_taskList));
+        if (askForExtraTasks(taskList, commRank, commSize) < 1) {
+            break;
+        }
+        else {
+            pthread_mutex_lock(&(taskList->mutex_taskList));
+            remainedTasksCount = taskList->remainedTasksCount;
+            pthread_mutex_unlock(&(taskList->mutex_taskList));
+        }
     }
 
     clock_gettime(CLOCK_REALTIME, &tm_finish);
 
     time = tm_finish.tv_sec - tm_start.tv_sec + 1e-9 * (tm_finish.tv_nsec - tm_start.tv_nsec);
+
+//    if (time < 1) {
+//        printf("IM HERE");
+//        if (askForExtraTasks(taskList, commRank, commSize) < 1) {
+//
+//            /////////////////////////////////////////////////////
+//            printf("%d TASK ARRAY: ", commRank);
+//            for (int i = 0; i < 10; i++) {
+//                printf(" %d ", taskList->taskList[i]);
+//            }
+//            printf("\n");
+//            printf("TIME: %lf\n", time);
+//            printf("TaskList remained: %d\n", taskList->remainedTasksCount);
+//            /////////////////////////////////////////////////////
+//        }
+//    }
+
 
     *workTime_return = time;
     return result;
@@ -149,7 +203,7 @@ void createAndStartTaskPullThread(pthread_t *thread_taskPull, TaskList * taskLis
         abort();
     }
 
-    if (0 != pthread_create(thread_taskPull, &attrs, pullTasks, NULL)) {
+    if (0 != pthread_create(thread_taskPull, &attrs, sendTasks, taskList)) {
         perror("Cannot create a thread");
         MPI_Finalize();
         abort();
@@ -157,6 +211,84 @@ void createAndStartTaskPullThread(pthread_t *thread_taskPull, TaskList * taskLis
 
     pthread_attr_destroy(&attrs);
 }
-void * pullTasks(void * taskListPointer) {
+
+void * sendTasks(void * taskListPointer) {
     TaskList * taskList = (TaskList *)taskListPointer;
+
+    int requestRank = QUIT_THREAD;
+    int sendTasksCount;
+    int * sendTaskListPointer;
+    int commRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &commRank);
+
+    while (1) {
+        MPI_Recv(&requestRank, 1, MPI_INT, MPI_ANY_SOURCE, TAG_REQUEST, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+        if (requestRank == QUIT_THREAD) {
+            break;
+        }
+        else {
+            pthread_mutex_lock(&(taskList->mutex_taskList));
+            if (taskList->remainedTasksCount >= MIN_REMAINED_TASKS_TO_START_SHARING) {
+
+                sendTasksCount = taskList->remainedTasksCount / 2;
+                sendTaskListPointer = taskList->taskList + sendTasksCount;
+                if (taskList->remainedTasksCount % 2) sendTaskListPointer += 1;
+                taskList->remainedTasksCount -= sendTasksCount;
+                MPI_Send(&sendTasksCount, 1, MPI_INT, requestRank, TAG_ANSWER, MPI_COMM_WORLD);
+                MPI_Send(sendTaskListPointer, sendTasksCount, MPI_INT, requestRank, TAG_ANSWER, MPI_COMM_WORLD);
+            }
+            else {
+                sendTasksCount = 0;
+                MPI_Send(&sendTasksCount, 1, MPI_INT, requestRank, TAG_ANSWER, MPI_COMM_WORLD);
+
+            }
+            pthread_mutex_unlock(&(taskList->mutex_taskList));
+
+//            /////////////////////////////////////////////////////
+//            printf("I, %d, MY TASK ARRAY: ", commRank);
+//            for (int i = 0; i < taskList->taskListLength; i++) {
+//                printf(" %d ", taskList->taskList[i]);
+//            }
+//            printf("\n");
+//            /////////////////////////////////////////////////////
+//            printf("I, %d,  SEND %d tasks TO PROC %d, my REMAIED TASKS %d  \n", commRank, sendTasksCount, requestRank, taskList->remainedTasksCount);
+        }
+
+    }
+
+    return NULL;
+}
+
+int askForExtraTasks(TaskList *taskList, int commRank, int commSize) {
+    int answer = -1;
+    int sendRank = commRank;
+
+    for (int i = (commRank + 1) % commSize; i != commRank; i = (i + 1) % commSize) {
+        if (i == commRank) break;
+        MPI_Send(&sendRank, 1, MPI_INT, i, TAG_REQUEST, MPI_COMM_WORLD);
+        MPI_Recv(&answer, 1, MPI_INT, i, TAG_ANSWER, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        if (answer > 0) {
+            pthread_mutex_lock(&(taskList->mutex_taskList));
+
+            MPI_Recv(taskList->taskList, answer, MPI_INT, i, TAG_ANSWER, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+//            /////////////////////////////////
+//            printf("I, %d, I RECEIVE: ", commRank);
+//            for (int i = 0; i < taskList->taskListLength; i++) {
+//                printf(" %d ", taskList->taskList[i]);
+//            }
+//            printf("\n");
+//            ////////////////////////////////////
+
+            taskList->remainedTasksCount = answer;
+            pthread_mutex_unlock(&(taskList->mutex_taskList));
+
+            return answer;
+        }
+//        else {
+//            printf("I, %d, DIDNT GET TASKS FROM PROC %d. Answer: %d\n", commRank, i, answer);
+//        }
+    }
+
+    return 0;
 }
